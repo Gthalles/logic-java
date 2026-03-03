d7254f539f6a17610f18f7501f434d7da9422a56
Gthalles <garbelottithalles@gmail.com>
2026-03-03 00:26:29 -0300

    chore: change primitive type declaration to var

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,28 @@ void main() {
     Array arrUtil = new Array();
 
     int[] intArr = {55, 44, 33, 22, 11};
-    Integer[] integerArr = {55, 44, 33, 22, 11, 55};
     double[] arr = {1, 2, 3, 4.44, -4.43};
+    Integer[] integerArr = {55, 44, 33, 22, 11, 55};
 
-    double sum = arrUtil.getSum(arr);
-    IO.println("Sum array elements: " + sum);
+    var sumResult = arrUtil.getSum(arr);
+    IO.println("Sum array elements: " + sumResult);
 
-    double biggest = arrUtil.findBiggest(arr);
-    IO.println("Biggest number in array: " + biggest);
+    var biggestNumber = arrUtil.findBiggest(arr);
+    IO.println("Biggest number in array: " + biggestNumber);
 
-    int evens = arrUtil.countEven(intArr);
-    IO.println("Count evens: " + evens);
+    var evensCount = arrUtil.countEven(intArr);
+    IO.println("Occurrence of evens numbers: " + evensCount);
 
-    int[] reversedArray = arrUtil.reverseArr(intArr);
+    var reversedArray = arrUtil.reverseArr(intArr);
     IO.println("Reversed array: " + Arrays.toString(reversedArray));
 
-    Set<Integer> unique = arrUtil.removeDuplicates(integerArr);
-    IO.println("Unique array: " + unique);
+    var arrHashSet = arrUtil.removeDuplicates(integerArr);
+    IO.println("Array of unique elements: " + arrHashSet);
 
-    Integer secondBiggest = arrUtil.findSecondBiggestNumber(intArr);
-    IO.println("Second biggest number in array: " + secondBiggest);
+    var secondBiggestNumber = arrUtil.findSecondBiggestNumber(intArr);
+    IO.println("Second biggest number in array: " + secondBiggestNumber);
 
-    int onlyPositiveNumbersSum = arrUtil.sumOnlyPositiveNumber(intArr);
-    IO.println("Only positive numbers sum: " + onlyPositiveNumbersSum);
+    var positiveOnlyNumbersSum = arrUtil.sumOnlyPositiveNumber(intArr);
+    IO.println("Only positive numbers sum: " + positiveOnlyNumbersSum);
 }
 
